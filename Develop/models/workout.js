574de54fc02890b97139cdf293489ff5a615da3d
Randy Faison <randyfaison@gmail.com>
2021-06-29 11:20:00 -0400

    types added to model

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -4,27 +4,39 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
 day: {
-
+    type: Date,
+    default: ()=> new Date()
 },
+
+exercises: [{
 type: {
-
+    type: String
 },
+
 name: {
-
+    type: String
 },
+
 duration: {
-
+    type: Number
 },
+
 weight: {
-
+    type: Number
 },
+
 reps: {
-
+    type: Number
 },
+
 sets: {
+    type: Number
+},
 
+distance: {
+    type: Number
 }
-
+}]
 });
 
 const workout = mongoose.model('workout', workoutSchema);;
