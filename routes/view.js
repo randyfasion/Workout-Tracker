ac06bf1efe14c204b75b99628bd90a0f1c7e3de2
Randy Faison <randyfaison@gmail.com>
2021-07-08 10:05:48 -0400

    stats route

--- routes/view.js
+++ routes/view.js
@@ -6,5 +6,8 @@ router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'))
 });
 
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
 
 module.exports = router;
